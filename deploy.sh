ee4d49d348382b3a97f39588c98d55a8867b0d50
Gaurav Sharma <gaurav.sharma.3@consultant.volvo.com>
2023-02-14 09:37:57 +0100

    build script

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+# build
+npm run build
+# navigate into the build output directory
+cd dist
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+git commit -m 'deploy'
+git push -f git@github.com:gs96222/funny-jokes-vue3-app.git main:gh-pages
+cd -
