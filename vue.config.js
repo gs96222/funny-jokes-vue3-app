7c393efb417321d0e8bbe3f7fefb37979efe4b1c
Gaurav Sharma <gaurav.sharma.3@consultant.volvo.com>
2023-02-14 09:33:05 +0100

    add script for deploying to github pages

---
+++ vue.config.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  publicPath:
+    // eslint-disable-next-line no-undef
+    process.env.NODE_ENV === "production" ? "/funny-jokes-vue3-app/" : "/",
+};
